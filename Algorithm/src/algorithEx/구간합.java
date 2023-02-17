949efaa52466b695b262e7acccc5c561810033cb
Hanbooyo <qnfudk@gmail.com>
2023-02-17 20:13:16 +0900

    구간합(2차원배열)

--- Algorithm/src/algorithEx/구간합.java
+++ Algorithm/src/algorithEx/구간합.java
@@ -14,19 +14,21 @@ public class 구간합 {
 		int N = Integer.parseInt(st.nextToken()); // case의 개수
 		
 		st = new StringTokenizer(br.readLine());
-		int A[] = new int[D];
-		for(int i=0; i<D; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+//		int A[] = new int[D];
+//		for(int i=0; i<D; i++) {
+//			A[i] = Integer.parseInt(st.nextToken());
+//		}
+		long[] S = new long[D+1];
+		for(int i=1; i<= D; i++) { 
+			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
 		}
+		
 		for(int j=0; j<N; j++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken()); // 시작 점
 			int end = Integer.parseInt(st.nextToken()); // 끝 점
-			int sum = 0;
-			for(int i=(start-1); i<end; i++) {
-				sum += A[i];
-			}
-			System.out.println(sum);
+
+			System.out.println(S[end] - S[start-1]);
 		}
 	}
 
